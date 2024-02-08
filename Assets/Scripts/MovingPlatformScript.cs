d55ae5e9c3a6365f156e256022252f770274446f
Anoosh K <55269680+Anoosh-p73@users.noreply.github.com>
2024-02-07 16:39:03 -0800

    Movement scripts

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class MovingPlatformScript : MonoBehaviour
 {
+
+    Vector3 defaultPos;
+    public float speed = 3.0f;  
+    public float amplitude = 2.0f; 
+
+    private float timeCounter = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        defaultPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*float pingpong = Mathf.PingPong(Time.time, 2);
+        transform.position = new Vector3(defaultPos.x + pingpong, defaultPos.y, defaultPos.z);*/
+        timeCounter += Time.deltaTime * speed;
+        float xPosition = Mathf.Lerp(-amplitude, amplitude, Mathf.SmoothStep(0.0f, 1.0f, Mathf.Sin(timeCounter)));
+
+        transform.position = new Vector3(defaultPos.x + xPosition, transform.position.y, transform.position.z);
+
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField] private float fullForce;
+    [SerializeField] private float continueForce;
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private float gravityMultiplier;
+
+    [SerializeField] private float velocityThreshold;
+
+    private bool isGround = true;
+    private float currVelocity = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            MoveForward();
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            MoveBackward();
+        if (Input.GetKey(KeyCode.Space))
+            Jump();
+    }
+
+    private void FixedUpdate() {
+        rb.AddForce(Vector3.down * jumpForce * gravityMultiplier, ForceMode.Acceleration);
+        currVelocity = rb.velocity.x;
+    }
+
+    void MoveForward() {
+        print("Going RIght");
+        // if(currVelocity < 0) 
+        if(currVelocity > velocityThreshold) rb.AddForce(Vector3.right * continueForce, ForceMode.Acceleration);
+        else rb.AddForce(Vector3.right * fullForce, ForceMode.Acceleration);
+    }
+    void MoveBackward() {
+        print("Going Left");
+        // if (currVelocity > 0) rb.AddForce(Vector3.left * fullForce, ForceMode.Impulse);
+        if (currVelocity < -velocityThreshold) rb.AddForce(Vector3.left * continueForce, ForceMode.Impulse);
+        else rb.AddForce(Vector3.left * fullForce, ForceMode.Impulse);
+    }
+
+    void Jump() {
+        print("Trying to jump");
+        if (!isGround) return;
+        print("Sucessfully jumped");
+        isGround = false;
+        //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
+
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.collider.tag == "Moving" || collision.collider.tag == "Stationary" || collision.collider.tag == "Ground") {
+            isGround = true;
+            Debug.Log("Grounded");
+        } else {
+            isGround = false;
+
+            Debug.Log("Not Grounded!");
+        }
+    }
+
+}
